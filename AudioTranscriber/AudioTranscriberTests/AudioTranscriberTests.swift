267e29a1fc9bc63399ae674a813855d638a70012
Jongho Lee <bazinga940@gmail.com>
2025-07-02 14:18:25 -0400

    Create Xcode project

---
+++ AudioTranscriber/AudioTranscriberTests/AudioTranscriberTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTranscriberTests.swift
+//  AudioTranscriberTests
+//
+//  Created by Jongho Lee on 7/2/25.
+//
+
+import Testing
+@testable import AudioTranscriber
+
+struct AudioTranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
